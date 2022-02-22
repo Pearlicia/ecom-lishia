02510dac6c8b4b154659d0990d1094e632e26670
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-22 14:11:34 +0100

    add user cart order and product models

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,6 @@
 const router = require('express').Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("user test");
-});
 
-router.post("/userposttest", (req, res) => {
-    const title = req.body.title;
-    // const message = req.body.message;
-    res.send("Title is: " + title);
-
-});
 
 
 
