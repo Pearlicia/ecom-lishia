35b4d01427eb3661eb08ec00042144ec8968a5dd
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-22 21:35:28 +0100

    implement login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 
 
@@ -8,12 +9,36 @@ router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.PASS_SEC).toString(),
     });
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username});
+        !user && res.status(401).json("Wrong Credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC
+        );
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        password !==req.body.password && 
+            res.status(401).json("Wrong Credentials!");
+            
+        res.status(200).json(user);
+    } catch (err){
         res.status(500).json(err);
     }
 });
