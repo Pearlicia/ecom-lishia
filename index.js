4b6257a7ef36a2dfbbf2c87947ca539adb29effe
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-22 15:13:02 +0100

    add new user to db

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 
 
 // Load env variables
@@ -16,9 +18,9 @@ mongoose
 });
 
 app.use(express.json());
-
-
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+
 
 
 app.listen(process.env.PORT || 5000, () => {
